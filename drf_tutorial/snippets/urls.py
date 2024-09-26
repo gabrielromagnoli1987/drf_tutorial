ab364edcc012b33e3090a3803fd3f4223e04b097
Gabriel <gabrielromagnoli1987@gmail.com>
2024-09-26 01:55:17 -0300

    uploading tutorial

---
+++ drf_tutorial/snippets/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from snippets import views
+
+# (function based views)
+# urlpatterns = [
+#     path('snippets/', views.snippet_list),
+#     path('snippets/<int:pk>/', views.snippet_detail),
+# ]
+
+# (class based views)
+urlpatterns = [
+    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('', views.api_root)
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
